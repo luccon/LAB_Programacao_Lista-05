b20568f4e54d23b263f1c79b8cfcb05704e04fc7
Luccon <lucas.barboza405@gmail.com>
2025-01-12 12:25:42 -0300

    Atividade

---
+++ q3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//Para finalizar o programa digite "-99"
+
+int main(){
+    int num = 0, menor = 0, aux = 0, sair = 0;
+    int *p_num = &num, *p_menor = &menor, *p_aux = &aux, *p_sair = &sair;
+
+    while(!sair){
+
+        printf("Digite o %dº número:\n",*p_aux+1);
+        scanf("%d",p_num);
+        if(*p_num == -99){
+        break;
+        }
+        if(aux == 0){
+            *p_menor = *p_num;
+        }
+        if(*p_num < *p_menor){
+            *p_menor = *p_num;
+        }
+        aux++;
+    }
+    printf("Programa encerrado. O menor número digitado foi: [%p] %d.\n",p_menor,*p_menor);
+}
