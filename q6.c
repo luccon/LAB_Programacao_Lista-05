e02f12315fe80bc7523a5df0c6f1ca96438a6444
Luccon <lucas.barboza405@gmail.com>
2025-01-12 14:27:26 -0300

    Atividade

---
+++ q6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define TAM 100
+
+void CopiaString(char*, char*);
+
+int main(){ 
+    char string[TAM];
+    char string2[TAM];
+
+    printf("Digite uma string: ");
+    fgets(string, sizeof(string), stdin);
+
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] == '\n') {
+            string[i] = '\0';
+            break;
+        }
+    }
+
+    CopiaString(string,string2);
+
+    printf("Primeira string: %s\n",string);
+    printf("Segunda string: %s\n",string2);
+}
+
+void CopiaString(char *str1, char *str2) {
+    while (*str1 != '\0') {
+
+        *str2 = *str1;
+
+        str1++;
+        str2++;
+    }
+    *str2 = '\0';
+}
